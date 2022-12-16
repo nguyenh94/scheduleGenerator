570a3d422a0ff70b2a78836c97c8fa2eca0eb0ee
Huong Nguyen <huongbng@usc.edu>
2022-12-15 21:56:43 -0800

    set up home page route and main

---
+++ app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, render_template, request, session, url_for, Response, send_file
+import os
+import io
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+@app.route("/")
+def home():
+    '''route to the homepage'''
+    return render_template("index.html")
+
+@app.route('/<path:path>')
+def catch_all(path):
+    '''catch errors and redirect to home page'''
+    return redirect(url_for("home"))
+
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True)
+
